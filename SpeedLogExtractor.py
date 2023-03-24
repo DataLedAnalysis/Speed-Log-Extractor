f8bb91b5bc014e329e280e119a24863b34de16b4
DataLedAnalysis <abdul.w1337@gmail.com>
2023-03-24 12:52:12 +0000

    FinalScraper

--- SpeedLogExtractor.py
+++ SpeedLogExtractor.py
@@ -3,6 +3,8 @@ import pandas as pd
 from tkinter import messagebox as msg, filedialog as fd
 from datetime import date
 import sys
+from urllib.request import urlopen
+from bs4 import BeautifulSoup as bs
 
 #/home/abdul/Documents/Python/Speed-Log-Extractor/table.html
 
@@ -23,7 +25,7 @@ class myClass(tk.Frame):
         #btns frame as 2,1
         b_malts = tk.Button(self.btns_frame, text='Extract Maltesers\' log speed', command = self.maltsExtractor)
         b_malts.grid(row=2, column=1,padx =10)
-        b_4line = tk.Button(self.btns_frame, text='Extract 4Line\'s log speed',padx=10)
+        b_4line = tk.Button(self.btns_frame, text='Extract 4Line\'s log speed',padx=10, command = self.fourLineExtractor)
         b_4line.grid(row=2, column=2)
 
         b_cancel = tk.Button(parent, text = 'Quit', command =self.parent.destroy)
@@ -57,6 +59,46 @@ class myClass(tk.Frame):
         # self.dataF.to_csv(f_pth,index=False,sep=',')
             self.dataF.to_excel(f_pth, index=False)
             msg.showinfo(title='Maltesers',message='Maltesers log speed output saved. You may quit the program now.')
+
+    def fourLineExtractor(self):
+        self.valUrl = self.e.get()
+
+        if not re.search("^https?:\/\/",self.valUrl):
+            msg.showerror(message='Check URL again! Hint: must start with http',title='Invalid URL')
+            self.e.delete(0,tk.END)
+            self.fourLineExtractor
+        
+        Fourline_html = urlopen(self.valUrl)
+        soup = bs(Fourline_html)
+
+        tableCells = soup.find_all('tr',attrs={'class':['TableCell1','TableCell2']})
+
+        dirtyData = []
+
+        for tr in tableCells:
+            tds = tr.find_all('td')
+            for td in tds:
+                dirtyData.append(td.text)
+
+        cleanData = [dirtyData[x:x+13] for x in range(0,len(dirtyData),13)]
+
+        d = pd.DataFrame(cleanData)
+        df = pd.DataFrame(cleanData,columns =['Date','Start','Stop','Minutes','Actual Speed','Description','Steel Band','Flap Open','Bullnose Open','Product Sensor','x1','x2','x3'])
+        finalized_table = df.loc[:,'Date':'Product Sensor']
+    
+        d = date.today().strftime('%d%b%y')
+        fname = d + '_4Line'
+        f_pth = fd.asksaveasfilename(initialfile=fname,defaultextension='xlsx',filetypes=[('Excel','*.xlsx')])
+
+        if bool(f_pth) == False: #exit if quit is pressed
+            msg.showinfo(title='Quitting',message='You decided to quit this file dialog, exiting the program. \n\nNOTE: You need to re-run, if you want to reuse this program again.')
+            sys.exit()
+        else:    
+        # self.dataF.to_csv(f_pth,index=False,sep=',')
+            finalized_table.to_excel(f_pth, index=False)
+            msg.showinfo(title='4Line',message='4Line log speed output saved. You may quit the program now.')
+        
+    
 
 window = myClass(tk.Tk())
 window.mainloop()
