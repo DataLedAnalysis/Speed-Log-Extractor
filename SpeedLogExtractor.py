743d8f2a23a1bc1fc9c0e0c20b5c7eaed4cab6bd
DataLedAnalysis <abdul.w1337@gmail.com>
2023-03-24 12:28:57 +0000

    AddedBs4

--- SpeedLogExtractor.py
+++ SpeedLogExtractor.py
@@ -1,7 +1,8 @@
-import tkinter as tk
+import tkinter as tk, re
 import pandas as pd
 from tkinter import messagebox as msg, filedialog as fd
 from datetime import date
+import sys
 
 #/home/abdul/Documents/Python/Speed-Log-Extractor/table.html
 
@@ -11,22 +12,36 @@ class myClass(tk.Frame):
         super().__init__(parent)
         #create an entry widget
         self.parent = parent
-        self.parent.title('Hello')
+        self.parent.title('Create Log Speed Excel sheet from URL')
         
-        self.e = tk.Entry(parent) #using self.master produces text
-        self.e.grid(row=1, column=1)
+        self.e = tk.Entry(parent,width=100) #using self.master produces text
+        self.e.grid(row=1, column=1,pady=10)
 
-        b = tk.Button(parent, text='Maltesers', command = self.tableExtractor1)
+        self.btns_frame = tk.Frame(parent)
+        self.btns_frame.grid(row=2,column=1)
+
+        #btns frame as 2,1
+        b_malts = tk.Button(self.btns_frame, text='Extract Maltesers\' log speed', command = self.maltsExtractor)
+        b_malts.grid(row=2, column=1,padx =10)
+        b_4line = tk.Button(self.btns_frame, text='Extract 4Line\'s log speed',padx=10)
+        b_4line.grid(row=2, column=2)
+
         b_cancel = tk.Button(parent, text = 'Quit', command =self.parent.destroy)
+        b_cancel.grid(row=3, column=1, pady=10)
 
-        b.grid(row=3, column=1)
-        b_cancel.grid(row=3, column=2)
-    
-    def tableExtractor1(self):
+    def maltsExtractor(self):
+
         self.valUrl = self.e.get()
+
+        #url check
+        if not re.search("^https?:\/\/",self.valUrl):
+            msg.showerror(message='Check URL again! Hint: must start with http',title='Invalid URL')
+            self.e.delete(0,tk.END)
+            self.maltsExtractor
+       
         self.dataF = pd.read_html(self.valUrl)
         self.dataF = self.dataF[-1] #get last html table
-        
+
         new_header = self.dataF.iloc[0]
         self.dataF = self.dataF[1:] #headerless data
         self.dataF.columns = new_header
@@ -34,9 +49,14 @@ class myClass(tk.Frame):
         d = date.today().strftime('%d%b%y')
         fname = d + '_Maltesers'
         f_pth = fd.asksaveasfilename(initialfile=fname,defaultextension='xlsx',filetypes=[('Excel','*.xlsx')])
+
+        if bool(f_pth) == False: #exit if quit is pressed
+            msg.showinfo(title='Quitting',message='You decided to quit this file dialog, exiting the program. \n\nNOTE: You need to re-run, if you want to reuse this program again.')
+            sys.exit()
+        else:    
         # self.dataF.to_csv(f_pth,index=False,sep=',')
-        self.dataF.to_excel(f_pth, index=False)
-        self.master.destroy()
+            self.dataF.to_excel(f_pth, index=False)
+            msg.showinfo(title='Maltesers',message='Maltesers log speed output saved. You may quit the program now.')
 
 window = myClass(tk.Tk())
 window.mainloop()
